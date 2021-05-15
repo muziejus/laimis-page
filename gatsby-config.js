0d34e66517f97daa50e584688bca6fe944b6d6a3
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-15 14:13:13 -0400

    Initial look with Gatsby defaults.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `In Memory of Laimis`,
+    description: `Remembering our beloved Laimis Jurgis Arcilio de Sá Pereira, 1984–2021`,
+    author: `@muziejus`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
