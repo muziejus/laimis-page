7c25b788d77e1a56a2106cc30bbe8997b496872e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-19 22:48:51 -0400

    Rig up Jimmy Fund

---
+++ src/pages/jimmy-fund.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+const JimmyFund = () => {
+  return (
+    <Layout>
+      <Seo title="Jimmy Fund" />
+    </Layout>
+  );
+}
+
+export default JimmyFund;
