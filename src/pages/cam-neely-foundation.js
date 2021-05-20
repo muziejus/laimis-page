e8dd0e4ce8995666fc783adc454be013845d8972
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-20 17:17:35 -0400

    Finish up Cam Neely Foundation.

---
+++ src/pages/cam-neely-foundation.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+const CamNeelyFoundation = () => {
+  return (
+    <Layout>
+      <Seo title="Cam Neely Foundation" />
+    </Layout>
+  );
+}
+
+export default CamNeelyFoundation;
