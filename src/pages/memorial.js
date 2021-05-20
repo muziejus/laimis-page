9f61fd715113407b749943cfb578b5430e0782b0
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-05-19 23:08:27 -0400

    Add memorial.

---
+++ src/pages/memorial.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+
+const Memorial = () => {
+  return (
+    <Layout>
+      <Seo title="Memorial" />
+    </Layout>
+  );
+}
+
+export default Memorial;
